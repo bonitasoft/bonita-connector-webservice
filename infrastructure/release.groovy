44e50502f716166543a7ecdde4889e3deeba33b0
Romain Bioteau <romain.bioteau@bonitasoft.com>
2018-08-23 14:27:12 +0200

    Update release.groovy

--- infrastructure/release.groovy
+++ infrastructure/release.groovy
@@ -1,13 +1,16 @@
 timestamps {
-    ansiColor('xterm') {
         node {
             stage('Setup') {
                 checkout scm
             }
 
             stage('Release version') {
-                sh './mvnw -B release:prepare release:perform -Darguments="-DskipTests -DaltDeploymentRepository=internal.releases::default::http://repositories.rd.lan/maven/internal.releases"'
-            }
+                  withCredentials([usernamePassword(
+                            credentialsId: 'github',
+                            passwordVariable: 'GIT_PASSWORD',
+                            usernameVariable: 'GIT_USERNAME')]) {
+                        sh './mvnw -B release:prepare release:perform -Darguments="-DskipTests -DaltDeploymentRepository=internal.releases::default::http://repositories.rd.lan/maven/internal.releases"'
+                  }
+             }
         }
-    }
 }
